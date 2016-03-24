9d87f3b412d8ec16f4b2e24de12694467f771606
lichifeng <lichifeng@users.noreply.github.com>
2016-03-24 13:30:09 +0800

    fixed a spell mistake of theme website

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
     <div class="site-info text-center">
         <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'goule' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'goule' ), 'WordPress' ); ?></a>
         •
-        <a href="<?php echo esc_url( __( 'https://lichifeng.com/', 'goule' ) ); ?>"><?php printf( __( 'Using %s theme', 'goule' ), 'Goule' ); ?></a>
+        <a href="<?php echo esc_url( __( 'http://lichifeng.com/', 'goule' ) ); ?>"><?php printf( __( 'Using %s theme', 'goule' ), 'Goule' ); ?></a>
         •
         <?php wp_loginout(); ?>
     </div><!-- .site-info -->
